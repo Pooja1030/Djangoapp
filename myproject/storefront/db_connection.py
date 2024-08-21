71d979fec22ea22d220135e3c921bf4e3eb586fe
Pooja <poojasawant1030@gmail.com>
2024-08-22 00:37:09 +0530

    dockerization

--- myproject/storefront/db_connection.py
+++ myproject/storefront/db_connection.py
@@ -1,6 +1,18 @@
-import pymongo
+# import pymongo
 
-url = 'mongodb://localhost:27017'
-client = pymongo.MongoClient(url)
+# url = 'mongodb://localhost:27017'
+# client = pymongo.MongoClient(url)
 
-db = client['test_mongo']
+# db = client['test_mongo']
+
+from pymongo import MongoClient
+import os
+
+mongo_client = MongoClient(
+    host=os.getenv('MONGO_DB_HOST', 'mongo'),
+    port=int(os.getenv('MONGO_DB_PORT', 27017)),
+    username=os.getenv('MONGO_DB_USER'),
+    password=os.getenv('MONGO_DB_PASSWORD'),
+)
+
+db = mongo_client[os.getenv('MONGO_DB_NAME')]
--- myproject/storefront/views.py
+++ myproject/storefront/views.py
@@ -1,3 +1,62 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import PersonSerializer
+from .db_connection import db
+from bson import ObjectId
+
+person_collection = db['storefront']
+
+class PersonList(APIView):
+    def get(self, request):
+        persons = person_collection.find()
+        persons_list = []
+        for person in persons:
+            person['id'] = str(person.pop('_id'))
+            persons_list.append(person)
+        serializer = PersonSerializer(persons_list, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PersonSerializer(data=request.data)
+        if serializer.is_valid():
+            data = serializer.validated_data
+            person_collection.insert_one(data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class PersonDetail(APIView):
+    def get(self, request, id):
+        person = person_collection.find_one({"_id": ObjectId(id)})
+        if person:
+            person['id'] = str(person.pop('_id'))
+            serializer = PersonSerializer(person)
+            return Response(serializer.data)
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def put(self, request, id):
+        serializer = PersonSerializer(data=request.data)
+        if serializer.is_valid():
+            data = serializer.validated_data
+            person_collection.update_one(
+                {"_id": ObjectId(id)},
+                {"$set": data}
+            )
+            return Response(data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        result = person_collection.delete_one({"_id": ObjectId(id)})
+        if result.deleted_count > 0:
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+
+
+
+
+
 # from django.shortcuts import render, redirect, get_object_or_404
 # from .models import person_collection
 # from django.http import HttpResponse
@@ -59,116 +118,3 @@
 #     return render(request, 'delete_person.html', {'person': person})
 
 
-
-# from rest_framework import status
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
-# from bson import ObjectId
-# from .serializers import PersonSerializer
-# from .db_connection import db
-# from django.shortcuts import render
-
-# def homepage(request):
-#     return render(request, 'storefront/homepage.html')
-
-
-
-# person_collection = db['storefront']
-
-# class PersonList(APIView):
-#     def get(self, request):
-#         persons = person_collection.find()
-#         persons_list = []
-#         for person in persons:
-#             person['id'] = str(person.pop('_id'))
-#             persons_list.append(person)
-#         serializer = PersonSerializer(persons_list, many=True)
-#         return Response(serializer.data)
-
-#     def post(self, request):
-#         serializer = PersonSerializer(data=request.data)
-#         if serializer.is_valid():
-#             data = serializer.validated_data
-#             person_collection.insert_one(data)
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-# class PersonDetail(APIView):
-#     def get(self, request, id):
-#         person = person_collection.find_one({"_id": ObjectId(id)})
-#         if person:
-#             person['id'] = str(person.pop('_id'))
-#             serializer = PersonSerializer(person)
-#             return Response(serializer.data)
-#         return Response(status=status.HTTP_404_NOT_FOUND)
-
-#     def put(self, request, id):
-#         serializer = PersonSerializer(data=request.data)
-#         if serializer.is_valid():
-#             data = serializer.validated_data
-#             person_collection.update_one(
-#                 {"_id": ObjectId(id)},
-#                 {"$set": data}
-#             )
-#             return Response(data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-#     def delete(self, request, id):
-#         result = person_collection.delete_one({"_id": ObjectId(id)})
-#         if result.deleted_count > 0:
-#             return Response(status=status.HTTP_204_NO_CONTENT)
-#         return Response(status=status.HTTP_404_NOT_FOUND)
-
-
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from .serializers import PersonSerializer
-from .db_connection import db
-from bson import ObjectId
-
-person_collection = db['storefront']
-
-class PersonList(APIView):
-    def get(self, request):
-        persons = person_collection.find()
-        persons_list = []
-        for person in persons:
-            person['id'] = str(person.pop('_id'))
-            persons_list.append(person)
-        serializer = PersonSerializer(persons_list, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = PersonSerializer(data=request.data)
-        if serializer.is_valid():
-            data = serializer.validated_data
-            person_collection.insert_one(data)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class PersonDetail(APIView):
-    def get(self, request, id):
-        person = person_collection.find_one({"_id": ObjectId(id)})
-        if person:
-            person['id'] = str(person.pop('_id'))
-            serializer = PersonSerializer(person)
-            return Response(serializer.data)
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    def put(self, request, id):
-        serializer = PersonSerializer(data=request.data)
-        if serializer.is_valid():
-            data = serializer.validated_data
-            person_collection.update_one(
-                {"_id": ObjectId(id)},
-                {"$set": data}
-            )
-            return Response(data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, id):
-        result = person_collection.delete_one({"_id": ObjectId(id)})
-        if result.deleted_count > 0:
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        return Response(status=status.HTTP_404_NOT_FOUND)
